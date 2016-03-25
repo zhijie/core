fa629000e02448c201f3b7afad0ba29b40b115ec
László Németh <laszlo.nemeth@collabora.com>
2016-03-25 20:59:39 +0100

    process SfxBindings in idle

--- vcl/source/app/idle.cxx
+++ vcl/source/app/idle.cxx
@@ -20,6 +20,16 @@
 #include <vcl/idle.hxx>
 #include "saltimer.hxx"
 
+void Idle::SetDeletionFlags()
+{
+    // If no AutoIdle, then stop.
+    if ( !mbAuto )
+    {
+        mpSchedulerData->mbDelete = true;
+        mbActive = false;
+    }
+}
+
 void Idle::Invoke()
 {
     maIdleHdl.Call( this );
@@ -34,11 +44,13 @@ Idle& Idle::operator=( const Idle& rIdle )
 
 Idle::Idle( const sal_Char *pDebugName ) : Scheduler( pDebugName )
 {
+    mbAuto = false;
 }
 
 Idle::Idle( const Idle& rIdle ) : Scheduler(rIdle)
 {
     maIdleHdl = rIdle.maIdleHdl;
+    mbAuto = rIdle.mbAuto;
 }
 
 void Idle::Start()
@@ -74,6 +86,22 @@ sal_uInt64 Idle::UpdateMinPeriod( sal_uInt64 nMinPeriod, sal_uInt64 /* nTime */ 
         break;
     }
     return nMinPeriod;
+}
+
+AutoIdle::AutoIdle()
+{
+    mbAuto = true;
+}
+
+AutoIdle::AutoIdle( const AutoIdle& rIdle ) : Idle( rIdle )
+{
+    mbAuto = true;
+}
+
+AutoIdle& AutoIdle::operator=( const AutoIdle& rIdle )
+{
+    Idle::operator=( rIdle );
+    return *this;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
