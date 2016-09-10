6a5bf6cf0b03390e1f8518a0934db23567443b40
Michael Meeks <michael.meeks@collabora.com>
2016-09-10 23:55:10 +0100

    Correct watchdog initialization, and extend timeouts for old hardware.
    Change-Id: Icda60b257655881ec1cee4621f1491cb1f011a38

--- vcl/source/opengl/OpenGLHelper.cxx
+++ vcl/source/opengl/OpenGLHelper.cxx
@@ -817,17 +817,17 @@ OpenGLWatchdogThread::OpenGLWatchdogThread()
 void OpenGLWatchdogThread::execute()
 {
     // delays to take various actions in 1/4 of a second increments.
-    static const int nDisableEntries[2] = { 6 /* 1.5s */, 20 /* 5s */ };
-    static const int nAbortAfter[2]     = { 20 /* 10s */, 120 /* 30s */ };
+    static const int nDisableEntries[2] = { 20 /* 5s */,  60 /* 15s */ };
+    static const int nAbortAfter[2]     = { 40 /* 10s */, 240 /* 60s */ };
 
     int nUnchanged = 0; // how many unchanged nEnters
-    TimeValue aHalfSecond(0, 1000*1000*1000*0.25);
+    TimeValue aQuarterSecond(0, 1000*1000*1000*0.25);
     bool bAbortFired = false;
 
     do {
         sal_uInt64 nLastEnters = OpenGLZone::gnEnterCount;
 
-        osl_waitCondition(gpWatchdogExit, &aHalfSecond);
+        osl_waitCondition(gpWatchdogExit, &aQuarterSecond);
 
         if (OpenGLZone::isInZone())
         {
